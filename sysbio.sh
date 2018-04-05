a8f75604c16a861d71a49a441545413d987e1d2f
Je-Hoon Song <song.jehoon@gmail.com>
2018-04-05 15:41:03 +0900

    .

--- sysbio.sh
+++ sysbio.sh
@@ -33,15 +33,9 @@ jup(){
 
 source $(dirname $0)/argparse.bash || exit 1
 argparse "$@" <<EOF || exit 1
-
 parser.add_argument('mode', type=str, help='build|start|stop|restart|shell')
-
 parser.add_argument('-f', '--foreground', action='store_true', default=False,
     help='whether foreground mode or not [default %(default)s]')
-
-# parser.add_argument('-m', '--multiple', nargs='+',
-#                     help='multiple values allowed')
-
 EOF
 
 case "$MODE" in 
